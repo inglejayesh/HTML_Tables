b14bebf5eaa108e7e30d22d54958fcc888874d99
Jayesh <inglejayesh28@gmail.com>
2022-04-23 01:08:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ var initialPersonalDetails = [
     fname: 'Jayesh', lname: 'Ingle', city: 'Shegaon', country: 'India'
   },
   {
-    fname: 'Srikant', lname: 'Katla', city: 'Mumbai', country: 'India'
+    fname: 'Srikant', lname: 'Katla', city: 'New York', country: 'U.S.A'
+  },
+  {
+    fname: 'Nishan', lname: 'Surya', city: 'Dubai', country: 'U.A.E'
+  },
+  {
+    fname: 'Vipul', lname: 'Thakre', city: 'Dubai', country: 'U.A.E'
   }
 ];
 
@@ -19,8 +25,8 @@ function loadInitialData() {
 
 
 function loadDetails() {
-  var table = document.getElementsByTagName('table')[0];
-  table.innerHTML = '<tr><th>SL NO</th><th>First Name</th><th>Last Name</th><th>City</th><th>Country</th>	</tr>';
+  var table = document.getElementById('detailsTable');
+  table.innerHTML = '<tr><th>SL No</th><th>First Name</th><th>Last Name</th><th>City</th><th>Country</th>	</tr>';
   for (i = 0; i < personalDetails.length; i++) {
     var newRow = table.insertRow(i + 1);
     newRow.insertCell(0).innerHTML = i + 1;
@@ -35,8 +41,10 @@ function addRowTop() {
   const formDetails=validateFormData();
   if(formDetails.isValid)
   {
+    // insert at index 0 in array
     personalDetails.unshift(formDetails.data);
-    loadDetails();  
+    loadDetails();
+    clearForm();
   }
 }
 
@@ -44,48 +52,80 @@ function addRowBottom() {
   const formDetails=validateFormData();
   if(formDetails.isValid)
   {
+    // insert at end in array
     personalDetails.push(formDetails.data);
     loadDetails();  
+    clearForm();
   }
 
 }
 
+function clearForm() {
+  document.getElementById('fname').value = '';
+  document.getElementById('lname').value = '';
+  document.getElementById('city').value = '';
+  document.getElementById('country').value = '';
+}
+
 function validateFormData()
 {
-  var table = document.getElementsByTagName('table')[0];
   var _fname = document.getElementById('fname').value;
   var _lname = document.getElementById('lname').value;
   var _city = document.getElementById('city').value;
   var _country = document.getElementById('country').value;
+  let _isValid = true;
 
-  /*
-  let _isValid = true
-  clear all error message (setempty)innerHtml
-  const regEx = 
-  if(fnameNotValid==false)
-  {
-    set error message fname is not valid
+  document.getElementById('errorInFname').innerHTML = '';
+  document.getElementById('errorInLname').innerHTML = '';
+  document.getElementById('errorInCity').innerHTML = '';
+  document.getElementById('errorInCountry').innerHTML = '';
+  document.getElementById('errorInForm').innerHTML = '';
+  
+  var nameRegx = /^[a-zA-Z]+$/;
+  var cityRegx = /^([a-zA-Z]+[\s]*){1,5}$/;
+  var countryRegx = /^([a-zA-Z]+\.?[\s]*){1,10}$/;
+  if (_fname.trim() == "") {
+    document.getElementById('errorInFname').innerHTML = '*Blank are not allowed';
+    _isValid = false;
+  } else if (_fname.length > 100 || !nameRegx.test(_fname)) {
+    document.getElementById('errorInFname').innerHTML = "*Please Enter Valid First Name";
     _isValid = false;
   }
-  if(lnameNotValid)
-  {
-    set error message lname is not valid
+
+  if (_lname.trim() == "") {
+    document.getElementById('errorInLname').innerHTML = '*Blank are not allowed';
+    _isValid = false;
+  } else if (_lname.length > 100 || !nameRegx.test(_lname)) {
+    document.getElementById('errorInLname').innerHTML = "*Please Enter Valid Last Name";
     _isValid = false;
   }
-  if(cityNotValid)
-  {
-    set error message city is not valid
+
+  if (_city.trim() == "") {
+    document.getElementById('errorInCity').innerHTML = '*Blank are not allowed';
+    _isValid = false;
+  } else if (_city.length > 100 || !cityRegx.test(_city)) {
+    document.getElementById('errorInCity').innerHTML = "*Please Enter Valid City Name";
     _isValid = false;
   }
-  if(countryNotValid)
-  {
-    set error message country is not valid
+
+  if (_country.trim() == "") {
+    document.getElementById('errorInCountry').innerHTML = '*Blank are not allowed';
+    _isValid = false;
+  } else if (_country.length > 100 || !countryRegx.test(_country)) {
+    document.getElementById('errorInCountry').innerHTML = "*Please Enter Valid Country Name";
     _isValid = false;
   }
-  */
+
+  if(_isValid == true) {
+    const data = personalDetails.find(x => x.fname.toLowerCase() == _fname.toLowerCase() && x.lname.toLowerCase() == _lname.toLowerCase() && x.city.toLowerCase() == _city.toLowerCase() && x.country.toLowerCase() == _country.toLowerCase())
+    if(data != null) {
+      document.getElementById('errorInForm').innerHTML = "*Data is already present.";
+      _isValid = false;
+    }
+  }
 
   const formDetails={
-    isValid:true,
+    isValid: _isValid,
     data:{fname: _fname, lname: _lname, city: _city, country: _country}
   }
   return formDetails;
